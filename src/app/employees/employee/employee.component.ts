48cf87c1bf2f0485be76be873f895caa5a2c153a
Imarshana <33378182+Imarshana@users.noreply.github.com>
2018-04-28 09:36:28 +0530

    Angular5crud with Firebase
    using firbase database we create crud oparation using angular5

---
+++ src/app/employees/employee/employee.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+import { EmployeeService } from '../shered/employee.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor( private employeeService : EmployeeService ) { }
+
+  ngOnInit() {
+    this.employeeService.getData();
+    this.resetForm();
+  }
+
+  onSubmit(employeeForm : NgForm){
+    this.employeeService.insertEmployee(employeeForm.value);
+    this.resetForm(employeeForm);
+
+  }
+  resetForm(employeeForm? : NgForm){
+    if(employeeForm !=null)
+    employeeForm.reset();
+    this.employeeService.selectedEmployee = {
+       $key : null,
+       name :'',
+       position :'',
+       office :'',
+       salary : 0
+    }
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyCvWCtT1TzUHnXAZmJOERah4uoGiw8aCGc",
+    authDomain: "angular5crud-a02d4.firebaseapp.com",
+    databaseURL: "https://angular5crud-a02d4.firebaseio.com",
+    projectId: "angular5crud-a02d4",
+    storageBucket: "angular5crud-a02d4.appspot.com",
+    messagingSenderId: "305992419694"
+  }
+};
